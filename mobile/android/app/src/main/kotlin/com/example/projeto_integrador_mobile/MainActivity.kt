9a10fa6fce127e2eacb20192e1bb4907834a412f
muriloAmachado <muriloandrademachado@gmail.com>
2026-06-15 11:06:07 -0300

    ajustando projeto flutter para executar mobile

---
+++ mobile/android/app/src/main/kotlin/com/example/projeto_integrador_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projeto_integrador_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- mobile/lib/core/config/api_config.dart
+++ mobile/lib/core/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:3000/api',
+    defaultValue: 'http://localhost:3000/api',
   );
 }
